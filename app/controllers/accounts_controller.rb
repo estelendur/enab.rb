45e3efe1eac9fa11140efbe9a77f9331197e9f38
Esty Thomas <kethomas2342@gmail.com>
2019-01-22 23:13:36 -0500

    Add some of the bits to create and view accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,49 @@
 class AccountsController < ApplicationController
+  def index
+    @accounts = Account.all
+  end
+
+  def show
+    @account = Account.find(params[:id])
+  end
+
+  def new
+    @account = Account.new
+  end
+
+  def edit
+    @account = Account.find(params[:id])
+  end
+
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      redirect_to @account
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @account = Account.find(params[:id])
+
+    if @account.update(account_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+
+    redirect_to accounts_path
+  end
+
+  private
+    def account_params
+      params.require(:account).permit(:name)
+    end
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,49 @@
 class TransactionsController < ApplicationController
+  def index
+    @transactions = Transaction.all
+  end
+
+  def show
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def edit
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    if @transaction.save
+      redirect_to @transaction
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @transaction = Transaction.find(params[:id])
+
+    if @transaction.update(transaction_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
+
+    redirect_to transactions_path
+  end
+
+  private
+    def transaction_params
+      params.require(:transaction).permit(:date, :account, :category, :amount, :memo)
+    end
 end
