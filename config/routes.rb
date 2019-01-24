6d7876fc4ca9083af8f18b226c9aadc7581d47f5
Esty Thomas <esty@healpay.com>
2019-01-24 00:26:40 -0500

    Making progress on accounts and transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :transactions
-  resources :accounts
-  resources :categories
+  resources :accounts do
+    resources :transactions
+  end
+  resources :categories do
+    resources :transactions
+  end
   get 'budget/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'budget#index'
 end
