1fecfeeda10aa835ee67dc2de198f38825985f65
Esty Thomas <estelendur@gmail.com>
2019-02-04 22:13:46 -0500

    MVP achieved!!

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,4 +1,10 @@
 class BudgetController < ApplicationController
   def index
+    @accounts = Account.all
+    @categories = Category.all
+    
+    @total_money = @accounts.map{|a| a.balance}.reduce(:+)
+    @allocated_money = @categories.map{|c| c.allocation}.reduce(:+)
+    @remaining_money = @total_money - @allocated_money
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,7 @@
 class Account < ApplicationRecord
   has_many :transactions
+
+  def balance
+    return self.transactions.map{|t| t.amount}.reduce(:+)
+  end
 end
