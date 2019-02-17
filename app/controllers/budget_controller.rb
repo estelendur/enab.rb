4d69137675e1af50a6433cd39a0de59c86a56c57
Esty Thomas <esty@healpay.com>
2019-02-16 23:23:30 -0500

    Display everything in table partials

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,8 +1,9 @@
 class BudgetController < ApplicationController
   before_action :authenticate_user!
   def index
-    @accounts = Account.where(user_id: current_user.id) || []
-    @categories = Category.where(user_id: current_user.id) || []
+    @accounts = Account.where(user_id: current_user.id).order(:name) || []
+    @categories = Category.where(user_id: current_user.id).order(:name) || []
+    @transactions = Transaction.where(user_id: current_user.id).order(:date, :amount)
     
     @total_money = @accounts.map{|a| a.balance}.reduce(:+) || 0
     @allocated_money = @categories.map{|c| c.allocation}.reduce(:+) || 0
