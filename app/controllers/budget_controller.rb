ab784c23af81899048d79096195f12eb236aae36
Esty Thomas <esty@healpay.com>
2019-02-17 21:18:45 -0500

    Add 'last 30 days spend' column to category table
    Excludes initial balance

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -5,13 +5,8 @@ class BudgetController < ApplicationController
     @categories = Category.where(user_id: current_user.id).order(:due_date, :name) || []
     @transactions = Transaction.where(user_id: current_user.id).order(date: :desc, amount: :asc)
     
-    @total_money = @accounts.map{|a|
-      if a.on_budget
-        a.balance
-      else
-        0
-      end}.reduce(:+) || 0
-    @allocated_money = @categories.map{|c| c.allocation}.reduce(:+) || 0
+    @total_money = @accounts.map{|a| if a.on_budget then a.balance else 0 end}.reduce(:+) || 0
+    @allocated_money = @categories.map{|c| c.allocation || 0}.reduce(:+) || 0
     @remaining_money = @total_money - @allocated_money
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,16 @@
 class Category < ApplicationRecord
   has_many :transactions
+
+  def last_30_days
+    if transactions.name.downcase == 'initial balance'
+      return 0
+    end
+    transactions = Transaction.where "category_id = ? AND created_at > ?", self.id, (Date::today - 30)
+    unless transactions.length > 0
+      return 0
+    end
+    return transactions
+      .map{|t| if t.expense then -t.amount else t.amount end}
+      .reduce(:+)
+  end
 end
