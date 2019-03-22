0bfd6bd48f916f6ee78a1fe0de640589c79ebe41
Esty Thomas <esty@healpay.com>
2019-03-22 00:44:31 -0400

    Add savings rate projections to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -14,4 +14,48 @@ class Category < ApplicationRecord
       .map { |t| t.expense ? t.amount : 0 }
       .reduce(:+)
   end
+
+  def save_daily
+    self.money_left / self.days_until_goal
+  end
+
+  def save_weekly
+    self.money_left / self.weeks_left
+  end
+
+  def save_biweekly
+    self.money_left / self.biweeks_left
+  end
+
+  def save_semimonthly
+    self.money_left / self.semimonths_left
+  end
+
+  def save_monthly
+    self.money_left / self.months_left
+  end
+
+  def days_until_goal
+    (self.due_date - Date.today).to_i
+  end
+
+  def money_left
+    self.goal_amount - self.allocation
+  end
+
+  def weeks_left
+    self.days_until_goal / 7
+  end
+
+  def biweeks_left
+    self.days_until_goal / 14
+  end
+
+  def semimonths_left
+    self.days_until_goal / 15
+  end
+
+  def months_left
+    self.days_until_goal / 30
+  end
 end
